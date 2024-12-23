2a892cd0624109ab36088b65b0489ebca8c82e63
ehdnd_mac <taeungjang@naver.com>
2024-12-23 17:13:30 +0900

    17-13-1 선언과 동시에 재정의

--- C0/17-13-1.c
+++ C0/17-13-1.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+// 재정의 전의 자료형 사용필여 없다면..
+// 형 선언과 동시에 재정의
 typedef struct {
   int num;
   double grade;
 } Student;
+
+// 기본 자료형도 재정의 가능
+typedef unsigned int nbyte;
 
 void print_data(Student* ps) {
   printf("학번: %d\n", ps->num);
