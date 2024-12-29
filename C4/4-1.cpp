e87fe20817b1b793b82ddf0530b1232a0a1e2f30
ehdnd_desktop <taeungjang@naver.com>
2024-12-30 00:51:50 +0900

    4-1 이 세상은 객체로 이루어져 있다.

--- C4/4-1.cpp
+++ C4/4-1.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// 객체란? 변수들과 참고 자료들로 이루어진 소프트웨어 덩어리
 class Animal {
   // 접근 지시자
  private:
@@ -28,6 +29,7 @@ class Animal {
 int main() {
   // 클래스를 이용해서 만들어진 객체: 인스턴스
   Animal animal;
+  // play 함수에 animal 을 인자로 주지 않아도 된다..
   animal.set_animal(100, 50);
   animal.increase_food(30);
 
